dedfcdb71950cfc22906f5fffc2aa1dca5050ae8
Trisha Rathod <trisharathod423@gmail.com>
2025-09-09 12:04:37 +0530

    main

---
+++ server/index.js
@@ -0,0 +1,137 @@
+// const nacl = require("tweetnacl");
+// const crypto = require("crypto");
+
+// function generateKeyPairs() {
+//   const signingKeyPair = nacl.sign.keyPair();
+//   const { privateKey, publicKey } = crypto.generateKeyPairSync('x25519', {
+//     modulusLength: 2048,
+//     publicKeyEncoding: {
+//       type: 'spki',
+//       format: 'pem',
+//     },
+//     privateKeyEncoding: {
+//       type: 'pkcs8',
+//       format: 'pem',
+//     },
+//   });
+
+//   return {
+//     Signing_private_key: Buffer.from(signingKeyPair.secretKey).toString(
+//       "base64"
+//     ),
+//     Signing_public_key: Buffer.from(signingKeyPair.publicKey).toString(
+//       "base64"
+//     ),
+//     Encryption_Privatekey: privateKey.toString('utf-8')
+//       .replace(/-----BEGIN PRIVATE KEY-----/, '')
+//       .replace(/-----END PRIVATE KEY-----/, '')
+//       .replace(/\s/g, ''),
+//     Encryption_Publickey: publicKey.toString('utf-8')
+//       .replace(/-----BEGIN PUBLIC KEY-----/, '')
+//       .replace(/-----END PUBLIC KEY-----/, '')
+//       .replace(/\s/g, ''),
+//   };
+// }
+
+// const keyPairs = generateKeyPairs();
+// console.log(keyPairs);
+
+// Import required modules
+const express = require('express'); // Express framework for handling HTTP requests
+const bodyParser = require('body-parser'); // Middleware for parsing request bodies
+const crypto = require('crypto'); // Node.js crypto module for encryption and decryption
+const _sodium = require('libsodium-wrappers');
+
+const port = 3000; // Port on which the server will listen
+const ENCRYPTION_PRIVATE_KEY =
+  'MC4CAQAwBQYDK2VuBCIEIHA+jwRt3qb7iISfxBgvJh5rrLjfEoI7i873grc7BBRq';
+const ONDC_PUBLIC_KEY =
+  'MCowBQYDK2VuAyEARa7vZyqavgLTlQg+TUKjhgqG2uvE9LkMEhoh9dGhQzo=';
+const REQUEST_ID = '6a6abf53-674f-4d6d-a52b-62e3fda55e03';
+const SIGNING_PRIVATE_KEY =
+  'T9e7d6aNJD1D90Y9qETlJGg0xLr0IuTKuMv6yg51CrwSxGy4nVCuYiZNz9nPVJOxUparuh3rKvj9mlyVzFRvrg==';
+
+const htmlFile = `
+<!--Contents of ondc-site-verification.html. -->
+<!--Please replace SIGNED_UNIQUE_REQ_ID with an actual value-->
+<html>
+  <head>
+    <meta
+      name="ondc-site-verification"
+      content="SIGNED_UNIQUE_REQ_ID"
+    />
+  </head>
+  <body>
+    ONDC Site Verification Page
+  </body>
+</html>
+`;
+// Pre-defined public and private keys
+const privateKey = crypto.createPrivateKey({
+  key: Buffer.from(ENCRYPTION_PRIVATE_KEY, 'base64'), // Decode private key from base64
+  format: 'der', // Specify the key format as DER
+  type: 'pkcs8', // Specify the key type as PKCS#8
+});
+const publicKey = crypto.createPublicKey({
+  key: Buffer.from(ONDC_PUBLIC_KEY, 'base64'), // Decode public key from base64
+  format: 'der', // Specify the key format as DER
+  type: 'spki', // Specify the key type as SubjectPublicKeyInfo (SPKI)
+});
+
+// Calculate the shared secret key using Diffie-Hellman
+const sharedKey = crypto.diffieHellman({
+  privateKey: privateKey,
+  publicKey: publicKey,
+});
+
+// Create an Express application
+const app = express();
+app.use(bodyParser.json()); // Middleware to parse JSON request bodies
+
+// Route for handling subscription requests
+app.post('/on_subscribe', function (req, res) {
+  const { challenge } = req.body; // Extract the 'challenge' property from the request body
+  const answer = decryptAES256ECB(sharedKey, challenge); // Decrypt the challenge using AES-256-ECB
+  const resp = { answer: answer };
+  res.status(200).json(resp); // Send a JSON response with the answer
+});
+
+// Route for serving a verification file
+app.get('/ondc-site-verification.html', async (req, res) => {
+  const signedContent = await signMessage(REQUEST_ID, SIGNING_PRIVATE_KEY);
+  // Replace the placeholder with the actual value
+  const modifiedHTML = htmlFile.replace(/SIGNED_UNIQUE_REQ_ID/g, signedContent);
+  // Send the modified HTML as the response
+  res.send(modifiedHTML);
+});
+
+// Default route
+app.get('/', (req, res) => res.send('Hello World!'));
+
+// Health check route
+app.get('/health', (req, res) => res.send('Health OK!!'));
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+
+// Decrypt using AES-256-ECB
+function decryptAES256ECB(key, encrypted) {
+  const iv = Buffer.alloc(0); // ECB doesn't use IV
+  const decipher = crypto.createDecipheriv('aes-256-ecb', key, iv);
+  let decrypted = decipher.update(encrypted, 'base64', 'utf8');
+  decrypted += decipher.final('utf8');
+  return decrypted;
+}
+
+async function signMessage(signingString, privateKey) {
+  await _sodium.ready;
+  const sodium = _sodium;
+  const signedMessage = sodium.crypto_sign_detached(
+    signingString,
+    sodium.from_base64(privateKey, _sodium.base64_variants.ORIGINAL)
+  );
+  const signature = sodium.to_base64(
+    signedMessage,
+    _sodium.base64_variants.ORIGINAL
+  );
+  return signature;
+}
