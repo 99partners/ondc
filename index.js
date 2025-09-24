4c4540077480fee9186cc5c4b5a7fbb4ecfb63b0
Mansi <mansigangani126@gmail.com.com>
2025-09-24 12:24:46 +0530

    path

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 // This file exists to override the main entry point specified in package.json
 // It simply imports and runs our server.js file
 
-require('./ondc-seller-search/server.js');
+require('./server.js');
